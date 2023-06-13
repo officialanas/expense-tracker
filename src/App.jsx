42b2b99ef8613c3a40a28224ba46636b407beddf
Anas Ansari <itsofficial177@gmail.com>
2023-06-13 18:01:29 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseTable from "./components/ExpenseTable";
+import expenseData from "./expenseData";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+
+function App() {
+  const [expense, setExpense] = useLocalStorage("expense", {
+    title: "",
+    category: "",
+    amount: "",
+  });
+  const [expenses, setExpenses] = useLocalStorage("expenses", expenseData);
+  const [editingRowId, setEditingRowId] = useLocalStorage("editingRowId", "");
+
+  //console.log(localData);
+
+  return (
+    <main>
+      <h1>Track Your Expense</h1>
+
+      <div className="expense-tracker">
+        <ExpenseForm
+          setExpenses={setExpenses}
+          expense={expense}
+          setExpense={setExpense}
+          editingRowId={editingRowId}
+          setEditingRowId={setEditingRowId}
+        />
+        <ExpenseTable
+          expenses={expenses}
+          setExpense={setExpense}
+          setExpenses={setExpenses}
+          setEditingRowId={setEditingRowId}
+        />
+      </div>
+    </main>
+  );
+}
+
+export default App;
